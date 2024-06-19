326007bc24a9b92db6f5ed74ea83c9cb3c43a498
Yusaku Mizobuchi <49979513+misonikomipan@users.noreply.github.com>
2024-06-20 01:58:36 +0900

    [Add] search-by-ingredients method

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,12 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+from recipi_generator import generate_recipi
+from XML_parser import parse_recipi
+
+
+class Ingredients(BaseModel):
+    ingredients: list[str]
 
 app = FastAPI()
 
@@ -6,3 +14,9 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.post("/recipes/search-by-ingredients")
+async def search_by_ingredients(ingredeints: Ingredients):
+    recipis_xml = generate_recipi(ingredeints.ingredients)
+    recipis_json = parse_recipi(recipis_xml)
+    return recipis_json
