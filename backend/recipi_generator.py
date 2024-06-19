89875be4215c6086bbf3228f08afdc5853a90854
Yusaku Mizobuchi <49979513+misonikomipan@users.noreply.github.com>
2024-06-20 01:57:31 +0900

    [Update] recipi_generator
    API用にリファクタリング

--- backend/recipi_generator.py
+++ backend/recipi_generator.py
@@ -7,7 +7,7 @@ client = Groq(
 )
 
 
-ja_prompt = f"""
+prompt = f"""
 回答は日本語でおねがいします。
 あなたのタスクは、提供された食材リストに基づいてレシピを提案することです。私が食材を提供するので、その食材を使ったレシピを考えてください。
 
@@ -45,61 +45,53 @@ ja_prompt = f"""
 </recipe>
 注意点:
 回答は日本語で提供してください。
+recipe_descriptionは、ユーザーが興味を持つような、レシピの魅力を伝える文章にしてください。
 レシピの指示は非常に詳細にしてください。
-調理時間、温度、技術、各材料の切り方、各食材を加えるタイミングなどを明確に記載してください。
+各材料の切り方、調理時間、温度、技術、各食材を加えるタイミングなどを明確に記載してください。
 レシピで使用する食材の量は、そのレシピが作る分量に対して適切であることを確認してください。
 ブレインストーミング、スクラッチパッド、最終レシピを含む完全な回答を<result>タグ内に提供してください。
 <result>
 ...
 </result>
-
 """
 
-def chat_with_groq():
-    print("Welcome to the Groq Chatbot! Type 'exit' to end the conversation.")
+def generate_recipi(ingredients: list[str]):
+    # print("Welcome to the Groq Chatbot! Type 'exit' to end the conversation.")
 
     # Initialize chat history with the initial prompt
     chat_history = [
-        {"role": "system", "content": ja_prompt}
+        {"role": "system", "content": prompt}
     ]
 
-    while True:
-        user_input = input(f"材料を「、」区切りで入力: ")
+    # Add the user's message to the chat history
+    chat_history.append({"role": "user", "content": "、".join(ingredients)})
 
-        if user_input.lower() == 'exit':
-            print("Goodbye!")
-            break
+    # Record the start time
+    # last_time = time.time()
 
-        # Add the user's message to the chat history
-        chat_history.append({"role": "user", "content": user_input})
+    # Create the completion request with the chat history
+    stream = client.chat.completions.create(
+        messages=chat_history,
+        model="llama3-70b-8192",
+        temperature=0.8,
+        max_tokens=10000,
+        top_p=1,
+        stop=None,
+        stream=True,
+    )
 
-        # Record the start time
+    # Print the incremental deltas returned by the LLM.
+    ai_response = ""
+    print(f"responce: ", end="")
+    for chunk in stream:
+        if chunk.choices[0].delta.content:
+            ai_response += chunk.choices[0].delta.content
+            print(chunk.choices[0].delta.content, end="")
         # last_time = time.time()
 
-        # Create the completion request with the chat history
-        stream = client.chat.completions.create(
-            messages=chat_history,
-            model="llama3-70b-8192",
-            temperature=0.5,
-            max_tokens=1024,
-            top_p=1,
-            stop=None,
-            stream=True,
-        )
-
-        # Print the incremental deltas returned by the LLM.
-        ai_response = ""
-        print(f"responce: ", end="")
-        for chunk in stream:
-            if chunk.choices[0].delta.content:
-                ai_response += chunk.choices[0].delta.content
-                print(chunk.choices[0].delta.content, end="")
-            # last_time = time.time()
-        print()
-        print()
-
-        # Add the AI's response to the chat history
-        chat_history.append({"role": "assistant", "content": ai_response})
+    # Add the AI's response to the chat history
+    chat_history.append({"role": "assistant", "content": ai_response})
+    return ai_response
 
 if __name__ == "__main__":
-    chat_with_groq()
+    generate_recipi(["にんじん", "ジャガイモ", "玉ねぎ"])
