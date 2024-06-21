a2df0f8f70d9c9230ac6743a5eded058c2440cdb
KEIDOROa <keidorpvp@icloud.com>
2024-06-21 19:32:27 +0900

    a

--- react/src/components/RecipiDisplay.tsx
+++ react/src/components/RecipiDisplay.tsx
@@ -34,38 +34,3 @@ export const RecipiDisplay: React.FC<RecipiDisplayProps> = ({content}) => {
   </>
   )
 }
-/*勉強メモ
-//importに{}　複数の細かいやつ
-//export default RecipiDisplay import{}いらない 一つのでかいやつ
-mapを使うと関数を繰り返し読み込ませることができる的な奴
-例
-1回目
-```
-item:
-{
-  img: "https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg",
-  title: "タイトルテキスト",
-  text: "ここにはレシピ紹介の文が入ります。"
-}
-  index: 0
-```
-2回目
-```
-item:
-{
-  img: "https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg",
-  title: "Title2",
-  text: "Text2"
-}
-  index: 1
-```
-React.FC<RecipiDisplayProps> はRecipiDisplayコンポーネントがRecipiDisplayPropsの型のpropsを受け取れる
-RecipiDisplayProps型はcontentプロパティがContentData型の配列であることを定義
-``` RecipiDisplayProps 型の定義
-type RecipiDisplayProps = {
-  content: ContentData[];
-};
-```
-なぜmapでkeyがあったほうがいい？
-パフォーマンス向上など
-*/
