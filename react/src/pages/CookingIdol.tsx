555772ebffe7c251d3d8fa7b4c50a50a9f4ab854
KEIDOROa <keidorpvp@icloud.com>
2024-06-22 13:18:06 +0900

    Revert "コンフリクト解消"
    This reverts commit 262afc4c74229a07b55d54527d5f04eb3edfe1eb, reversing
    changes made to c39fa6c19718be8fa06081c905ed4131d9a16948.

--- react/src/pages/CookingIdol.tsx
+++ react/src/pages/CookingIdol.tsx
@@ -1,11 +1,9 @@
 import { Header } from '@/components/Header'
-import InputForm from '@/components/InputForm';
 import { MakingDisplay } from '@/components/MakingDisplay';
 import { RecipiDisplay } from '@/components/RecipiDisplay'
 import axios from 'axios';
-import React, { useState, useEffect } from 'react';
+import React,{useState, useEffect} from 'react';
 import { text } from 'stream/consumers';
-
 type IngredientData = {
   name: string;
   amount: string;
@@ -22,28 +20,49 @@ type TitleData = {
   title:string;
 };
 
-
-
 const Cookingidol = () => {
-  const [ingredients, setIngredients] = useState<string>('');
-  const [content, setContent] = useState<ContentData[]>([]);
+  const [content, setcontent] = useState<ContentData[]>([]);
   const [maikingcontent, setmaikingcontent] = useState<ContentData[]>([]);
   const [item, setitem] = useState<ContentData[]>([]);
-const handleReceiveData = (data: TitleData[]) => {
-  const selectedRecipe = content.find(recipe => recipe.title === data[0].title);
-  if (selectedRecipe) {
-    setmaikingcontent([selectedRecipe]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try{// レシピデータの取得
+        //const response = await axios.get('http://localhost:8000/');
+        const response = await axios.post(`http://localhost:8000/recipes/search-by-ingredients`,{
+          ingredients:["じゃがいも","にんじん"]
+        });
+        const apiRecipes = response.data.recipes.map((recipe: any) => ({
+          title: recipe.recipe_name,//レシピ名
+          text: recipe.recipe_description,//レシピの説明
+          ingredients: recipe.ingredients.map((ing: any) => ({
+            name: ing.ingredient_name,
+            amount: ing.ingredient_amount
+          })),
+          instructions:recipe.instructions//手順
+        }));
+        setcontent(apiRecipes);
+        setitem(apiRecipes);
+        //setmaikingcontent(response.data)
+      }catch(error){
+        console.error('リクエストエラー:', error); 
+      }
+    };
+  fetchData();
+  },[]);
+
+  const handleReceiveData = (data: TitleData[]) => {
+    const selectedRecipe = content.find(recipe => recipe.title === data[0].title);
+    if (selectedRecipe) {
+      setmaikingcontent([selectedRecipe]);
+    }
+  };
+    return (
+      <div>
+        <Header />
+        <RecipiDisplay content={content} onReceiveData={handleReceiveData}/>
+        <MakingDisplay maikingcontent={maikingcontent}/>
+      </div>
+    )
   }
-};
 
-  return (
-    <div>
-      <Header />
-      <InputForm setContent={setContent} />
-      <RecipiDisplay content={content} onReceiveData={handleReceiveData}/>
-      <MakingDisplay maikingcontent={maikingcontent} />
-    </div>
-  )
-}
-
-export default Cookingidol
+  export default Cookingidol
