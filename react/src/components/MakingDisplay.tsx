5787f96cac61c5627f260d0dd97a5d0b578ba13b
Yusaku Mizobuchi <49979513+misonikomipan@users.noreply.github.com>
2024-06-23 13:27:39 +0900

    [Add] image gen featrue (but sample data)

--- react/src/components/MakingDisplay.tsx
+++ react/src/components/MakingDisplay.tsx
@@ -16,20 +16,21 @@ type ContentData = {
   instructions: string[];
 };
 
-type MakingDisplayProps = {
-  makingContent: ContentData[];
+type ImageUrlObj = {
+  sample: string;
+  newProp?: string;  // 新しいプロパティをオプショナル（必須ではない）として追加
 };
 
-interface ImageUrlObj {
-  [prop: string]: any
-}
+type MakingDisplayProps = {
+  makingContent: ContentData[];
+  recipeImageUrls: ImageUrlObj;
+};
 
 
-export const MakingDisplay: React.FC<MakingDisplayProps> = ({ makingContent }) => {
+
+export const MakingDisplay: React.FC<MakingDisplayProps> = ({ makingContent, recipeImageUrls }) => {
   const [musicUrl, setMusicUrl] = useState<string>('');
   const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [recipeImageUrl, setRecipeImageUrl] = useState<ImageUrlObj>({})
-
   const handleReceiveData = (url: string) => {
     setMusicUrl(url);
     setIsLoading(false);
@@ -43,28 +44,7 @@ export const MakingDisplay: React.FC<MakingDisplayProps> = ({ makingContent }) =
     <>
       {makingContent.map((item, index) => (
         <div key={index}>
-<<<<<<< Updated upstream
-          <div className='making width center'>
-            <h3 className='center title-jp textcenter'>{item.title}</h3>
-            <p className='text center width'>{item.text}</p>
-            <h3 className='center title-jp textcenter'>材料</h3>
-            <div>
-              <ul className='wrap width center'>
-                {item.ingredients.map((ingredient, i) => (
-                  <li key={i} className='text two'>{ingredient.name}: {ingredient.amount}</li>
-                ))}
-              </ul>
-            </div>
-            <h3 className='center title-jp textcenter'>手順</h3>
-            <div>
-              <ol className='center width'>
-                {item.instructions.map((instruction, i) => (
-                  <li key={i} className='text'>{instruction}</li>
-                ))}
-              </ol>
-            </div>
-=======
-          {recipeImageUrl[item.title] && <img src={recipeImageUrl[item.title]}></img>}
+          {recipeImageUrls[item.title] && <img src={recipeImageUrls[item.title]}></img>}
           <div className='making-container'>
             <h3 className="center title-jp">{item.title}</h3>
             <p>{item.text}</p>
@@ -81,7 +61,6 @@ export const MakingDisplay: React.FC<MakingDisplayProps> = ({ makingContent }) =
                 <li key={i}>{instruction}</li>
               ))}
             </ol>
->>>>>>> Stashed changes
           </div>
           <MusicGenerateButton 
             title={item.title} 
